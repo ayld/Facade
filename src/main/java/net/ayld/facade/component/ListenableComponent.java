6d551cc504787b805d2b04ad772e4569918a610a
ayld <iliev.simeon@gmail.com>
2013-09-26 18:26:56 +0300

    added documentation to ListenableComponent

--- src/main/java/net/ayld/facade/component/ListenableComponent.java
+++ src/main/java/net/ayld/facade/component/ListenableComponent.java
@@ -1,9 +1,24 @@
 package net.ayld.facade.component;
 
+import net.ayld.facade.event.model.ClassResolverUpdate;
+import net.ayld.facade.event.model.JarExtractionStartUpdate;
+import net.ayld.facade.event.model.SourceResolverUpdate;
+
 import org.springframework.beans.factory.annotation.Required;
 
 import com.google.common.eventbus.EventBus;
 
+/**
+ * Intended for extension.
+ * 
+ * Extenders should use the given event bus to notify listeners for progress updates.
+ * The definition of what a 'progress update' is depends on the context of the extender.
+ * 
+ * @see ClassResolverUpdate
+ * @see JarExtractionStartUpdate
+ * @see SourceResolverUpdate
+ * @see StatusUpdate
+ * */
 public abstract class ListenableComponent {
 
 	protected EventBus eventBus;
